122e57702d054b7db4501ebc77df9f1aa29cb2b9
hubcio2115 <kowalski.hubercik1@gmail.com>
2023-10-12 17:07:37 +0200

    refactor of zad03

--- lab01/src/main/java/org/example/zad03/Zad03.java
+++ lab01/src/main/java/org/example/zad03/Zad03.java
@@ -1,20 +1,9 @@
 package org.example.zad03;
 
-import java.util.Map;
-
 public class Zad03 {
-  private static final double YEAR_ON_EARTH_IN_SECONDS = 31557600;
-  private static final Map<Planets, Double> YEAR_ON_PLANETS_IN_SECONDS =
-      Map.of(
-          Planets.MERCURY, 0.2408467 * YEAR_ON_EARTH_IN_SECONDS,
-          Planets.VENUS, 0.61519726 * YEAR_ON_EARTH_IN_SECONDS,
-          Planets.MARS, 1.8808158 * YEAR_ON_EARTH_IN_SECONDS,
-          Planets.JUPYTER, 11.862615 * YEAR_ON_EARTH_IN_SECONDS,
-          Planets.SATURN, 29.447498 * YEAR_ON_EARTH_IN_SECONDS,
-          Planets.URANUS, 84.016846 * YEAR_ON_EARTH_IN_SECONDS,
-          Planets.NEPTUNE, 164.79132 * YEAR_ON_EARTH_IN_SECONDS);
+  public static final double YEAR_ON_EARTH_IN_SECONDS = 31557600;
 
-  public static double zad03(int age, Planets planet) {
-    return age * YEAR_ON_EARTH_IN_SECONDS * YEAR_ON_PLANETS_IN_SECONDS.get(planet);
+  public static double zad03(int age, Planet planet) {
+    return age * YEAR_ON_EARTH_IN_SECONDS * planet.getYear();
   }
 }
