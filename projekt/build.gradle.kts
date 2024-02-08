ffcb9487de061d09362f9ad571db1fee02f4e90c
hubcio2115 <kowalski.hubercik1@gmail.com>
2024-02-08 11:42:55 +0100

    final project

--- projekt/build.gradle.kts
+++ projekt/build.gradle.kts
@@ -20,6 +20,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.postgresql:postgresql")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
 
     compileOnly("org.projectlombok:lombok:1.18.30")
